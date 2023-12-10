52a3d6e3506233716881104cf983ce19c5edfe5a
CHAITHANYA4523 <devichaithanya45@gmail.com>
2023-12-10 22:25:43 +0530

    added  complexities

--- Arrays/array rotation.cpp
+++ Arrays/array rotation.cpp
@@ -18,3 +18,9 @@ vector<int> rotateArray(vector<int>&a, int x, string dir) {
     }
     return a;
 }
+
+/*
+The time complexity of this function is O(n), where n is the size of the array. This is because there are two for loops that each run n times, but they are not nested, so they do not multiply together.
+
+The space complexity of this function is also O(n). This is because a new vector v of size n is created to hold the rotated array. The rest of the variables use a constant amount of space.
+*/
