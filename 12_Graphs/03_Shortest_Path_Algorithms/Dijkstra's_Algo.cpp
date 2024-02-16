33b9453b762cc514a9f5a542661da4626022fc10
CHAITHANYA4523 <devichaithanya45@gmail.com>
2024-02-16 22:48:47 +0530

    added set solution and changed comment

--- 12_Graphs/03_Shortest_Path_Algorithms/Dijkstra's_Algo.cpp
+++ 12_Graphs/03_Shortest_Path_Algorithms/Dijkstra's_Algo.cpp
@@ -31,8 +31,69 @@ class Solution{
 };
 
 /*
-- **Time Complexity**: The time complexity of this algorithm is O((V+E)log V), where V is the number of vertices and E is the number of edges in the graph. This is because, in the worst case, every vertex and every edge will be processed in the priority queue. Here, the operations include extracting the minimum from the priority queue and updating the distances of the vertices, which take logarithmic time.
+- **Time Complexity**: 
+source code
+    while(!pq.empty()){ -> O(V)
+                node = pq.top() -> O(log V)
+                for(iterating over adjacent nodes){ -> no.of edges (ne = V-1) most dense graph case where V-1 nodes are adjacent to a vertex
+
+                    if(condition){
+                        update distance
+                        pq.push(); -> O(log V)
+                    }
+                }
+            }
+    }
+
+ => O(V(pop from heap + no of edges on vertex * push into priority queue))
+ => O(V(log(heap size) + ne * log(heap size)))  in worst case ne = V-1 (dense graph)
+ => O(V(log(heap size)(ne + 1)))
+ => O(V(log(heap size)(V-1 + 1)))
+ => O(V(log(heap size)(V)))
+ => O(V^2(log(heap size))) heap size is equal to V^2 (dense graph each node has V-1 adjnodes)
+ => O(V^2(log(V^2))) v^2 = E because every node has V-1 adjacent nodes i.e V-1(V) ~ V^2
+ => O(E(2*log(V)))
 
 - **Space Complexity**: The space complexity is O(V), due to the distance array and the priority queue. In the worst case, all the nodes of the graph will be in the priority queue, so the space complexity is linear in the number of vertices V.
 */
+
+//Dikjstra using set for optimised complexity
+
+class Solution{
+    public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S){
+        //{distance, node}
+        set<pair<int,int>> s;
+        s.insert({0, S});
+        vector<int> dist(V, 1e9);
+        dist[S] = 0;
+        while(!s.empty()){
+            // s.begin() is an iterator so dereference it to get the values
+            auto it = *(s.begin());
+            int dis = it.first;
+            int node = it.second;
+            s.erase(it);
+            for(auto itr : adj[node]){
+                int adjNode = itr[0];
+                int edgeWeight = itr[1];
+                if(dis + edgeWeight < dist[adjNode]){
+                    //erase the pair from the set if the distance now obtained is 
+                    //smaller than previous distance 
+                    // if dist[adjNode] is not 1e9 then it is previously visited by some other node as it will overwrite 1e9
+                    if(dist[adjNode] != 1e9){
+                        s.erase({dist[adjNode], adjNode});
+                    }
+                    dist[adjNode] = dis + edgeWeight;
+                    s.insert({dist[adjNode], adjNode});
+                }
+            }
+        }
+        for(int i=0; i<V; i++){
+            if(dist[i] == 1e9) dist[i] = -1;
+        }
+        return dist;
+    }
+};
 
