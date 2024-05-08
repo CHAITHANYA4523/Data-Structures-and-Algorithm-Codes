24df53da983921b5ae6d5216881905bcbf47c014
CHAITHANYA4523 <devichaithanya45@gmail.com>
2024-05-08 22:48:32 +0530

    add large exponentiation

--- 15_Number_Theory/02_binary_exponentiation.cpp
+++ 15_Number_Theory/02_binary_exponentiation.cpp
@@ -52,3 +52,55 @@ Time Complexity: O(log b) - Similar to the recursive approach, the time complexi
 
 Space Complexity: O(1) - The iterative approach utilizes constant space regardless of the size of the exponent, as it doesn't rely on recursion and doesn't allocate additional space based on the exponent's magnitude.
 */
+
+// a <= 10^9, b <= 10^9, M <= 10^9
+
+//case: 1) a > 10^9
+
+// a <= 10^9 or 2^1024
+int binaryExp(int a, int b){
+    a = binExp(a, b);
+    int ans = 1;
+    while(b > 0){
+        if(b&1){
+            ans = (ans * 1LL * a)%M;
+        }
+        a = (a * 1LL * a)%M;
+        b = b >> 1;
+    }
+}
+
+//case: 2) M > 10^9
+
+// M <= 10^18 
+
+//in this case as M = 1e18 + 7 if a = 1e9, 
+//after this step a = (a * 1LL * a)%M;  a = 1e18 in next iteration 
+//it will overflow with 1e18 * 1e9
+long long binaryExp(long long a, long long b){
+    int ans = 1;
+    while(b > 0){
+        if(b&1){
+            ans = binMultiply(ans,a)%M;
+        }
+        a = binMultiply(a,a)%M;
+        b = b >> 1;
+    }
+}
+
+// a*a = a + a + .. + a (a times)
+long long binMultiply(long long a, long long b){
+    int ans = 0;
+    while(b > 0){
+        if(b&1){
+            ans = (ans + a)%M;
+        }
+        a = (a + a)%M;
+        b = b >> 1;
+    }
+    return ans;
+}
+
+/*Time complexity: log^2(n) (log square n).
+log(n) for binaryExp and n for binMultiply function
+*/
