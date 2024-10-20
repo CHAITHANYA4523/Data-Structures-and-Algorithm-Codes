fd2df2303d5a99f4fd94f4e7f9ad1f06283fce68
CHAITHANYA4523 <devichaithanya45@gmail.com>
2024-10-20 23:24:59 +0530

    add new code style

--- 06_Linked_lists/cloning_a_linked list.cpp
+++ 06_Linked_lists/cloning_a_linked list.cpp
@@ -105,3 +105,64 @@ public:
     }
 };
 
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+private:    
+    Node* dummy = new Node(-1);
+    void insertCopyNodes(Node* head){
+        Node *temp = head;
+        while(temp){
+            Node *nextNode = temp->next;
+            Node *copy = new Node(temp->val);
+            copy->next = temp->next;
+            temp->next = copy;
+            temp = nextNode;
+        }
+    }
+
+    void connectRandomPointers(Node *head){
+        Node *temp = head;
+        while(temp){
+            Node *copy = temp->next;
+            if(temp->random) copy->random = temp->random->next;
+            else copy->random = NULL;
+            temp = temp->next->next;
+        }
+    }
+
+    Node* getDeepCopy(Node *head){
+        Node *res = dummy;
+        Node *temp = head;
+        while(temp){
+            res->next = temp->next;
+            temp->next = temp->next->next;
+            res = res->next;
+            temp = temp->next;
+        }
+        return dummy->next;
+    }
+
+public:
+    Node* copyRandomList(Node* head) {
+        insertCopyNodes(head);
+        connectRandomPointers(head);
+        return getDeepCopy(head);
+    }
+};
+
